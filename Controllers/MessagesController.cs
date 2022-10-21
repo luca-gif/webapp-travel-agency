f9c2ef219f261ea66e46cc05e75a719f454a334d
luca-gif <luca.89m@gmail.com>
2022-10-21 17:50:35 +0200

    Aggiunta dashboard per i messaggi, è ora possibile anche rimuoverli

---
+++ Controllers/MessagesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webapp_travel_agency.Context;
+using webapp_travel_agency.Models;
+
+namespace webapp_travel_agency.Controllers
+{
+    public class MessagesController : Controller
+    {
+        private readonly AgencyContext _ctx = new AgencyContext();
+        public IActionResult Index()
+        {
+            AgencyContext _ctx = new AgencyContext();
+
+            List<Info> messages = _ctx.informazioni.ToList();
+
+            return View("Messages", messages);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            Info msgToDelete = _ctx.informazioni.Where(msg => msg.Id == id).FirstOrDefault();
+
+            if (msgToDelete != null)
+            {
+                _ctx.informazioni.Remove(msgToDelete);
+                _ctx.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
